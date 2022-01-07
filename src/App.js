0a756c30727f4a2b6c9a655e26caf9838aab27fb
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-01-07 16:52:20 +0200

    commit proj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import ShoppingList from "./components/ShoppingList";
+import { useState } from "react";
+
+import "./App.css";
+import { data } from "./components/data";
 
 function App() {
+  const { bookItems } = data;
+  const [bookItem, setBookItem] = useState([]);
+  const handleAddBook = (book) => {
+    const ListItem = bookItem.find((item) => item.id === book.id);
+    if (ListItem) {
+      setBookItem(
+        bookItem.map((item) =>
+          item.id === book.id
+            ? {
+                ...ListItem,
+                count: ListItem.count + 1,
+              }
+            : item
+        )
+      );
+    } else {
+      setBookItem([...bookItem, { ...book, count: 1 }]);
+    }
+  };
+  const handleRemoveBook = (book) => {
+    const ListItem = bookItem.find((item) => item.id === book.id);
+    if (ListItem.count === 1) {
+      setBookItem(bookItem.filter((item) => item.id !== book.id));
+    } else {
+      setBookItem(
+        bookItem.map((item) =>
+          item.id === book.id
+            ? { ...ListItem, count: ListItem.count - 1 }
+            : item
+        )
+      );
+    }
+  };
+  const handleDelete = (book) => {
+    const ListItem = bookItem.find((item) => item.id === book.id);
+    if (ListItem.count >= 1) {
+      setBookItem(bookItem.filter((item) => item.id !== book.id));
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header bookItems={bookItems} handleAddBook={handleAddBook} />
+      <ShoppingList
+        bookItem={bookItem}
+        handleAddBook={handleAddBook}
+        handleRemoveBook={handleRemoveBook}
+        handleDelete={handleDelete}
+      />
+    </>
   );
 }
 
